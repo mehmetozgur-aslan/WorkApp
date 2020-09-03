7aeddc7f4ff0bb8edbc9f8acf7eb1a2ec21cf8c2
Mehmet Özgür Aslan <mehmetozguraslan09@gmail.com>
2020-09-03 12:59:34 +0300

    Profil güncelleme

---
+++ YSKProje.ToDo.Web/Areas/Admin/Controllers/ProfileController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using YSKProje.ToDo.Entities.Concrete;
+using YSKProje.ToDo.Web.Areas.Admin.Models;
+
+namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public ProfileController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            TempData["menu"] = "profile";
+
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            AppUserListViewModel model = new AppUserListViewModel()
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Surname = user.Surname,
+                Email = user.Email,
+                Picture = user.Picture
+            };
+
+            return View(model);
+        }
+    }
+}
--- YSKProje.ToDo.Web/Areas/Admin/Models/AppUserListViewModel.cs
+++ YSKProje.ToDo.Web/Areas/Admin/Models/AppUserListViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,19 @@ namespace YSKProje.ToDo.Web.Areas.Admin.Models
     public class AppUserListViewModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Ad alanı zorunludur.")]
+        [Display(Name = "Ad: ")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyad alanı zorunludur.")]
+        [Display(Name = "Soyad: ")]
         public string Surname { get; set; }
+
+        [Display(Name="Email: ")]
         public string Email { get; set; }
+
+        [Display(Name = "Resim: ")]
         public string Picture { get; set; }
     }
 }
