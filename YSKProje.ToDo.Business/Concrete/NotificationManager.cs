50084385617b09478faebc2460a8bfca931700e5
Mehmet Özgür Aslan <mehmetozguraslan09@gmail.com>
2020-09-12 17:49:44 +0300

    Notification tablosu generic repo entegrasyonu

---
+++ YSKProje.ToDo.Business/Concrete/NotificationManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YSKProje.ToDo.Business.Interfaces;
+using YSKProje.ToDo.DataAccess.Interfaces;
+using YSKProje.ToDo.Entities.Concrete;
+
+namespace YSKProje.ToDo.Business.Concrete
+{
+    public class NotificationManager : INotificationService
+    {
+        private readonly INotificationDal _notificationDal;
+
+        public NotificationManager(INotificationDal notificationDal)
+        {
+            _notificationDal = notificationDal;
+        }
+
+        public void Delete(Notification entity)
+        {
+            _notificationDal.Delete(entity);
+        }
+
+        public List<Notification> GetAll()
+        {
+            return _notificationDal.GetAll();
+        }
+
+        public Notification GetById(int id)
+        {
+            return _notificationDal.GetById(id);
+        }
+
+        public void Save(Notification entity)
+        {
+            _notificationDal.Save(entity);
+        }
+
+        public void Update(Notification entity)
+        {
+            _notificationDal.Update(entity);
+        }
+    }
+}
